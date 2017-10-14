8cbd0d908253c7ae3fcbc66d488fb520dcc68c29
111111.txt <1040325106@qq.com>
2017-10-14 17:45:41 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <span>
+      <img src="./assets/logo (1).png"/>
+    </span>
+    <a href="#">下载APP</a>
+  </header>
+  
+</template>
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style src="./assets/index.css">
+
+</style>
